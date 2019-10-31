58d1a09be034959946f4a88a59494d1b53386d9b
nichia <nichia@gmail.com>
2019-10-31 12:45:47 -0400

    Studio 3.4 - add remove cheese function

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -15,12 +15,12 @@ import java.util.HashMap;
 @RequestMapping("cheese")
 public class CheeseController {
 
-    static HashMap<String, String> cheeses = new HashMap<>();
+    // static ArrayList<String> cheeses = new ArrayList<>();
+   static HashMap<String, String> cheeses = new HashMap<>();
 
     // Request path: /cheese
     @RequestMapping(value = "")
     public String index(Model model) {
-
         model.addAttribute("cheeses", cheeses);
         model.addAttribute("title", "My Cheeses");
         return "cheese/index";
@@ -32,11 +32,38 @@ public class CheeseController {
         return "cheese/add";
     }
 
-    @RequestMapping(value = "add", method = RequestMethod.POST)
+    // @RequestMapping(value = "add", method = RequestMethod.POST)
     // public String processAddCheeseForm(HttpServletRequest request) {
     // String cheeseName = request.getParameter("cheeseName");
-    public String processAddCheeseForm(@RequestParam String cheeseName, String cheeseDescription) {
+    // public String processAddCheeseForm(@RequestParam String cheeseName) {
+    //    cheeses.add(cheeseName);
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDescription) {
         cheeses.put(cheeseName, cheeseDescription);
+
+        // Redirect to /cheese
+        return "redirect:";
+    }
+
+
+    @RequestMapping(value = "remove", method = RequestMethod.GET)
+    public String displayRemoveCheeseForm(Model model){
+        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("title", "Remove Cheese");
+
+        return "cheese/remove";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.POST)
+    //Dropdown menu - select element
+    //public String processRemoveCheeseForm(@RequestParam String cheese) {
+            //cheeses.remove(cheese);
+    public String processRemoveCheeseForm(@RequestParam String[] cheese) {
+        for (String aCheese : cheese){
+                cheeses.remove(aCheese);
+        }
+
         // Redirect to /cheese
         return "redirect:";
     }
